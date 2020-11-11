34b59b4d4527bbd4e6400933fe1e98db41a9b70b
Børge André Jensen <borgeajensen@gmail.com>
2020-11-11 11:08:59 +0100

    Trend class

---
+++ GameJam/Trend.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameJam
+{
+    class Trend
+    {
+        public int TicksRemaining { get; private set; }
+        public Currency Currency { get; private set; }
+
+        private double oldMin;
+        private double oldMax;
+
+        public Trend(int howManyTicks, Currency currency, double trendWeight)
+        {
+            if (trendWeight < 0)
+            {
+                trendWeight = 0.01;
+            }
+
+            TicksRemaining = howManyTicks;
+            Currency = currency;
+            oldMin = currency.MinValue;
+            oldMax = currency.MaxValue;
+
+            currency.MinValue *= trendWeight;
+            currency.MaxValue *= trendWeight;
+        }
+
+        public void nextTick()
+        {
+            TicksRemaining--;
+
+            if (TicksRemaining == 0)
+            {
+                Currency.MinValue = oldMin;
+                Currency.MaxValue = oldMax;
+            }
+        }
+    }
+}
